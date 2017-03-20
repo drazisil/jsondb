4d68856f6fbd787e7ca4192163aca09bca259d48
Joseph W Becher <jwbecher@drazisil.com>
2017-03-20 16:15:56 -0400

    added create

--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,32 @@
 /* global it describe */
-require('should')
+/* eslint prefer-arrow-callback: 0 */
+/* eslint func-names: 0 */
+const chai = require('chai')
+const fs = require('fs')
 const jsondb = require('../lib/index.js')
 
-describe('moo', () => {
-  describe('moo!', () => {
-    it('should return moo', () => {
-      jsondb.moo().should.equal('moo')
+const databaseFilename = './db.json'
+
+const should = chai.should()
+
+describe('File handling', () => {
+  it('should be able to create a database file', (done) => {
+    fs.stat(databaseFilename, (errStat) => {
+      errStat.code.should.equal('ENOENT')
+      jsondb.create(databaseFilename, (e) => {
+        should.not.exist(e)
+        fs.stat(databaseFilename, (errStat2) => {
+          should.not.exist(errStat2)
+          fs.unlinkSync(databaseFilename)
+          done()
+        })
+      })
     })
   })
+
+  it('should be able to read a database file')
+
+  it('should be able to update a database file')
+
+  it('should be able to delete a database file')
 })
